43602804b98cf853c4e7a64aa312784fe9eb2533
borzykin <borzykin3455@gmail.com>
2019-03-10 23:46:52 +0200

    Update BaseTest.java

--- src/main/java/BaseTest.java
+++ src/main/java/BaseTest.java
@@ -1,3 +1,44 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.hamcrest.CoreMatchers.containsString;
+
+import java.util.concurrent.TimeUnit;
+
 public class BaseTest {
 
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.gecko.driver","src\\main\\resources\\geckodriver.exe");
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void basicTest() {
+        driver.get("https://google.com");
+        driver.findElement(By.name("q")).sendKeys("memes");
+        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+        WebDriverWait wait = new WebDriverWait(driver, 2);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("resultStats")));
+        String actualTitle = driver.getTitle();
+        Assert.assertThat(actualTitle, containsString("memes"));
+
+    }
 }
