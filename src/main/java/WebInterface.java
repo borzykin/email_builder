5b882d65ea9a50afdc3e716d6d5fd4da94130c43
ob <borzykin3455@gmail.com>
2019-09-04 22:50:50 +0300

    Also removed imports..

--- src/main/java/WebInterface.java
+++ src/main/java/WebInterface.java
@@ -7,10 +7,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.text.SimpleDateFormat;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
 
 public class WebInterface
